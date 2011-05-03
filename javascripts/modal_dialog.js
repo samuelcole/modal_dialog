4e0ce4e43a91990b78ea4326642184ad4580e06f
Samuel Cole <sam@samuelcole.name>
2011-05-03 18:58:41 -0400

    Pass JSLint.

--- javascripts/modal_dialog.js
+++ javascripts/modal_dialog.js
@@ -1,4 +1,4 @@
-(function($) {
+(function ($) {
 
 /*
  * Makes a link open a modal dialog.
@@ -63,301 +63,310 @@
  *                  than the screen), margin (in px) to show on the top and
  *                  bottom of the dialog. (default: 20)
  */
-$.fn.modal_dialog = function(options) {
-	options = options || {};
-	return $(this).each(function() {
-		var md = $(this).data('modal_dialog');
-		if (!md) {
-		  md = new ModalDialog($(this), options);
-			$(this).data('modal_dialog', md);
-		}
-		md.open(options.event);
-	});
-};
+  function ModalDialog($elem, options) {
+    this.$elem = $elem;
 
-// Map of seen remote dialogs to their ModalDialog objects
-window.seen_remote_urls = {};
+    this.$close_a = $elem.find('a.modal_dialog_close');
+    var $outer = $elem.children(),
+      $sizer = $outer.children(),
+      $inner = $sizer.children(),
 
-$.create_modal_dialog = function(title, body, options) {
-  if(title.length) {
-    //we don't want to create an empty h4 if there is no title
-    title = '<h4>' + title + '</h4>';
+      $close_link = $elem.add($outer).add($sizer).add($inner).add(this.$close_a);
+
+    this.options = $.extend({
+      close_link: $close_link,
+      scroll_margin: 20,
+      prevent_close: false
+    }, options);
+
+    this.prevent_close = this.options.prevent_close;
+    if (this.prevent_close) {
+      this.$close_a.hide();
+    }
+
+    this._remember_original_css();
+    this._attach();
   }
-  var $modal = $('<div class="modal_dialog" style="display: none;">' +
-    '<div class="modal_dialog_ie6_background"></div>' +
-    '<div class="modal_dialog_outer">' +
-      '<div class="modal_dialog_sizer">' +
-        '<div class="modal_dialog_inner">' +
-          '<div class="modal_dialog_content">' +
-            '<div class="modal_dialog_head">' +
-              title +
-              '<a class="modal_dialog_close" href="#"><span class="icon-x">Close</span></a>' +
-            '</div>' +
-            '<div class="modal_dialog_body">' +
-            body +
+
+  $.fn.modal_dialog = function (options) {
+    options = options || {};
+    return $(this).each(function () {
+      var md = $(this).data('modal_dialog');
+      if (!md) {
+        md = new ModalDialog($(this), options);
+        $(this).data('modal_dialog', md);
+      }
+      md.open(options.event);
+    });
+  };
+
+  // Map of seen remote dialogs to their ModalDialog objects
+  window.seen_remote_urls = {};
+
+  $.create_modal_dialog = function (title, body, options) {
+    if (title.length) {
+      //we don't want to create an empty h4 if there is no title
+      title = '<h4>' + title + '</h4>';
+    }
+
+    var $modal = $('<div class="modal_dialog" style="display: none;">' +
+      '<div class="modal_dialog_ie6_background"></div>' +
+      '<div class="modal_dialog_outer">' +
+        '<div class="modal_dialog_sizer">' +
+          '<div class="modal_dialog_inner">' +
+            '<div class="modal_dialog_content">' +
+              '<div class="modal_dialog_head">' +
+                title +
+                '<a class="modal_dialog_close" href="#"><span class="icon-x">Close</span></a>' +
+              '</div>' +
+              '<div class="modal_dialog_body">' +
+              body +
+              '</div>' +
             '</div>' +
           '</div>' +
-        '</div>'+
-        '<span class="modal_dialog_ie_hack"></span>' +
+          '<span class="modal_dialog_ie_hack"></span>' +
+        '</div>' +
       '</div>' +
-    '</div>' +
-  '</div>');
+      '</div>'),
 
-  $('body').append($modal);
+      md = new ModalDialog($modal, options);
 
-  md = new ModalDialog($modal, options);
+    $('body').append($modal);
 
-  $modal.data('modal_dialog', md);
+    $modal.data('modal_dialog', md);
 
-  return $modal;
-};
+    return $modal;
+  };
 
 
-function return_modal_dialog_from_remote(remote_url, options) {
-  var md;
+  function return_modal_dialog_from_remote(remote_url, options) {
+    var md,
+      $modal = $([]);
 
-  $.each(window.seen_remote_urls, function(seen_url) {
-    if(url_equals(remote_url, seen_url)) {
-      md = this;
-    }
-  });
-
-  if(!md) {
-    var $modal = $.create_modal_dialog(options.title || '', 'spinner', options);
-    $modal.addClass('spinning');
-    md = $modal.data('modal_dialog');
-    
-    md.remote_xhr = $.ajax({
-      url: remote_url,
-      type: 'get',
-      dataType: options.dataType,
-      beforeSend: options.beforeSend,
-      success: function(data) {
-        contents = data;
-        $modal.find('.modal_dialog_body').html(contents);
-        $modal.removeClass('spinning');
-        $modal.trigger('modal_dialog:remote_dialog_loaded');
-      },
-      error: function() {
-        $modal.remove();
-        delete window.seen_remote_urls[remote_url];
+    $.each(window.seen_remote_urls, function (seen_url) {
+      if (url_equals(remote_url, seen_url)) {
+        md = this;
       }
     });
 
-    md.remote_url = remote_url;
+    if (!md) {
+      $modal = $.create_modal_dialog(options.title || '', 'spinner', options);
+      $modal.addClass('spinning');
+      md = $modal.data('modal_dialog');
+      
+      md.remote_xhr = $.ajax({
+        url: remote_url,
+        type: 'get',
+        dataType: options.dataType,
+        beforeSend: options.beforeSend,
+        success: function (data) {
+          var $contents = $(data);
+          (options.remote_modal_dialog_contents_loaded || F).apply(this, [$contents, options, remote_url ]);
+          $modal.find('.modal_dialog_body').html($contents);
+          $modal.removeClass('spinning');
+          $modal.trigger('size_change');
+          $modal.trigger('modal_dialog:remote_dialog_loaded');
+        },
+        error: function () {
+          $modal.remove();
+          delete window.seen_remote_urls[remote_url];
+        }
+      });
 
-    window.seen_remote_urls[remote_url] = md;
+      md.remote_url = remote_url;
+
+      window.seen_remote_urls[remote_url] = md;
+    }
+
+    return md;
   }
 
-  return md;
-}
+  window.F = function () { }; //no-op
 
-$.open_remote_modal_dialog = function(remote_url, options) {
-	options = options || {};
-  md = return_modal_dialog_from_remote(remote_url, options);
-  md.open(options.event);
-  return md;
-};
+  $.open_remote_modal_dialog = function (remote_url, options) {
+    options = options || {};
+    var md = return_modal_dialog_from_remote(remote_url, options);
+    md.open(options.event);
+    return md;
+  };
 
 
-function ModalDialog($elem, options) {
-	this.$elem = $elem;
 
-	this.$close_a = $elem.find('a.modal_dialog_close');
-	var $outer = $elem.children();
-	var $sizer = $outer.children();
-	var $inner = $sizer.children();
+  $.extend(ModalDialog.prototype, {
+    _attach: function () {
+      var _this = this,
+        $close_link = $(this.options.close_link);
 
-	var $close_link = $elem.add($outer).add($sizer).add($inner).add(this.$close_a);
+      this.$elem.bind('close.modal_dialog', function () { _this.close(); });
+      this.$elem.bind('size_change.modal_dialog', function () { _this._on_size_changed(); });
+      this.$elem.bind('prevent_close.modal_dialog', function (e) { _this._set_prevent_close(true); });
+      this.$elem.bind('allow_close.modal_dialog', function (e) { _this._set_prevent_close(false); });
+      $(window).bind('resize.modal_dialog', function () { _this._on_size_changed(); });
 
-	this.options = $.extend({
-		close_link: $close_link,
-		scroll_margin: 20,
-    prevent_close: false
-	}, options);
+      $close_link.bind('click.modal_dialog', function (e) {
+        if ($close_link.index(e.target) !== -1) {
+          _this.close(e);
+          e.preventDefault();
+        }
+      });
 
-  this.prevent_close = this.options.prevent_close;
-  if (this.prevent_close) {
-    this.$close_a.hide();
-  }
+      this.$elem.keypress(function (e) {
+        if (e.keyCode === 0x1b) {
+          // Escape
+          _this.close(e);
+          e.preventDefault();
+        }
+      });
+    },
 
-	this._remember_original_css();
-	this._attach();
-}
+    _detach: function () {
+      $(this.options.close_link).unbind('click.modal_dialog');
+      this.$elem.unbind('size_change.modal_dialog');
+      $(window).unbind('resize.modal_dialog');
+    },
 
-$.extend(ModalDialog.prototype, {
-	_attach: function() {
-		var _this = this;
+    _on_size_changed: function (opening) {
+      var $inner = this.$elem.find('div.modal_dialog_inner');
+      if ($inner.height() > this._get_viewport_height()) {
+        this._resize_to_position_absolute(opening);
+      } else {
+        this._resize_to_position_fixed();
+      }
+    },
 
-		this.$elem.bind('close.modal_dialog', function() { _this.close(); });
-		this.$elem.bind('size_change.modal_dialog', function() { _this._on_size_changed(); });
-    this.$elem.bind('prevent_close.modal_dialog', function(e) { _this._set_prevent_close(true); });
-    this.$elem.bind('allow_close.modal_dialog', function(e) { _this._set_prevent_close(false); });
-		$(window).bind('resize.modal_dialog', function() { _this._on_size_changed(); });
+    _resize_to_position_fixed: function () {
+      this._apply_original_css();
+    },
 
-		var $close_link = $(this.options.close_link);
-		$close_link.bind('click.modal_dialog', function(e) {
-			if ($close_link.index(e.target) != -1) {
-				_this.close(e);
-				e.preventDefault();
-			}
-		});
+    // For when position: fixed would lead to an un-scrollable dialog, we instead
+    // show the dialog at the current position and let the user scroll down. The
+    // whole situation is quite confusing, code-wise.
+    _resize_to_position_absolute: function (opening) {
+      if ($.browser.msie && $.browser.version < '7') {
+        this.$elem[0].style.removeExpression('top');
+        this.$elem[0].style.removeExpression('height');
+      }
 
-		this.$elem.keypress(function(e) {
-			if (e.keyCode == 0x1b) {
-				// Escape
-				_this.close(e);
-				e.preventDefault();
-			}
-		});
-	},
+      var body_height = $('body').height(),
 
-	_detach: function() {
-		$(this.options.close_link).unbind('click.modal_dialog');
-		this.$elem.unbind('size_change.modal_dialog');
-		$(window).unbind('resize.modal_dialog');
-	},
+        // One of <body> or <html> will have a non-zero scrollTop if scrolled
+        // (safari: body, ie/ff: html)
+        scroll_top = $('body')[0].scrollTop + $('html')[0].scrollTop,
 
-	_on_size_changed: function(opening) {
-		var $inner = this.$elem.find('div.modal_dialog_inner');
-		if ($inner.height() > this._get_viewport_height()) {
-			this._resize_to_position_absolute(opening);
-    } else {
-      this._resize_to_position_fixed();
-		}
-	},
+        $inner = this.$elem.find('div.modal_dialog_inner'),
+        inner_height = $inner.height(),
+        margin = this.options.scroll_margin,
 
-	_resize_to_position_fixed: function() {
-		this._apply_original_css();
-	},
+        min_page_height = scroll_top + inner_height + margin * 2,
+        page_height = min_page_height > body_height ? min_page_height : body_height,
 
-	// For when position: fixed would lead to an un-scrollable dialog, we instead
-	// show the dialog at the current position and let the user scroll down. The
-	// whole situation is quite confusing, code-wise.
-	_resize_to_position_absolute: function(opening) {
-		if ($.browser.msie && $.browser.version < '7') {
-			this.$elem[0].style.removeExpression('top');
-			this.$elem[0].style.removeExpression('height');
-		}
+        margin_bottom = page_height - scroll_top - inner_height - (margin * 2),
 
-		var body_height = $('body').height();
+        was_absolute = this.$elem.css('position') === 'absolute';
 
-		// One of <body> or <html> will have a non-zero scrollTop if scrolled
-		// (safari: body, ie/ff: html)
-		var scroll_top = $('body')[0].scrollTop + $('html')[0].scrollTop;
+      this.$elem.css('position', 'absolute');
+      this.$elem.css('top', '0');
+      this.$elem.css('height', page_height);
+      if (!was_absolute || opening) {
+        $inner.css('margin-top', scroll_top + margin);
+        $inner.css('margin-bottom', margin_bottom);
+      }
+    },
 
-		var $inner = this.$elem.find('div.modal_dialog_inner');
-		var inner_height = $inner.height();
-		var margin = this.options.scroll_margin;
+    _remember_original_css: function () {
+      this._elem_original_css = {};
+      this._inner_original_css = {};
 
-		var min_page_height = scroll_top + inner_height + margin * 2;
-		var page_height = min_page_height > body_height ? min_page_height : body_height;
+      var $inner = this.$elem.find('div.modal_dialog_inner'),
+        i,
+        elem_props = ['position', 'top'],
+        inner_props = ['margin-top', 'margin-bottom'],
+        _this = this;
 
-    var margin_bottom = page_height - scroll_top - inner_height - (margin * 2);
+      $.each(elem_props, function (i) {
+        var prop = elem_props[i];
+        _this._elem_original_css[prop] = _this.$elem.css(prop);
+      });
 
-    var was_absolute = this.$elem.css('position') == 'absolute';
-		this.$elem.css('position', 'absolute');
-		this.$elem.css('top', '0');
-		this.$elem.css('height', page_height);
-    if (!was_absolute || opening) {
-      $inner.css('margin-top', scroll_top + margin);
-      $inner.css('margin-bottom', margin_bottom);
+      $.each(inner_props, function (i) {
+        var prop = inner_props[i];
+        _this._inner_original_css[prop] = $inner.css(prop);
+      });
+    },
+
+    _get_viewport_height: function () {
+      if ($.browser.msie) {
+        return window.document.documentElement.clientHeight;
+      } else {
+        return window.innerHeight;
+      }
+    },
+
+    _apply_original_css: function () {
+      var prop,
+        value,
+        $inner = this.$elem.find('div.modal_dialog_inner');
+
+      for (prop in this._elem_original_css) {
+        value = this._elem_original_css[prop];
+        this.$elem.css(prop, value);
+      }
+
+      for (prop in this._inner_original_css) {
+        value = this._inner_original_css[prop];
+        $inner.css(prop, value);
+      }
+
+      if ($.browser.msie && $.browser.version < '7') {
+        // setExpression() doesn't take effect immediately, sometimes
+        this.$elem[0].style.top = document.documentElement.scrollTop;
+        this.$elem[0].style.setExpression('top', 'eval(document.documentElement.scrollTop)');
+        this.$elem[0].style.height = document.documentElement.clientHeight;
+        this.$elem[0].style.setExpression('height', 'eval(document.documentElement.clientHeight)');
+      } else {
+        this.$elem.css('height', '100%');
+      }
+    },
+
+    _set_prevent_close: function (prevent_close) {
+      this.prevent_close = prevent_close;
+      if (prevent_close) {
+        this.$close_a.hide();
+      } else {
+        this.$close_a.show();
+      }
+    },
+
+    open: function (e) {
+      var $body = this.$elem.parents('body');
+      if ($body.length) {
+        $body.append(this.$elem);
+      }
+      this.$elem.show();
+      this._on_size_changed(true);
+
+      this.$elem.trigger('modal_dialog:opened', [e]);
+
+      this._attach();
+    },
+
+    close: function (e) {
+      if (this.prevent_close) {
+        return;
+      }
+
+      this.$elem.trigger("before_close.modal_dialog", [e]);
+
+      if (e && e.isDefaultPrevented()) {
+        return;
+      }
+
+      this._detach();
+
+      this.$elem.hide();
+
+      this.$elem.trigger("closed.modal_dialog", [e]);
     }
-	},
+  });
 
-	_remember_original_css: function() {
-		this._elem_original_css = {};
-		this._inner_original_css = {};
-
-		var $inner = this.$elem.find('div.modal_dialog_inner');
-		var i;
-		var elem_props = ['position', 'top'];
-		var inner_props = ['margin-top', 'margin-bottom'];
-
-    var _this = this;
-    $.each(elem_props, function(i) {
-			prop = elem_props[i];
-			_this._elem_original_css[prop] = _this.$elem.css(prop);
-		});
-
-    $.each(inner_props, function(i) {
-			prop = inner_props[i];
-			_this._inner_original_css[prop] = $inner.css(prop);
-		});
-	},
-
-	_get_viewport_height: function() {
-		if ($.browser.msie) {
-			return window.document.documentElement.clientHeight;
-		} else {
-			return window.innerHeight;
-		}
-	},
-
-	_apply_original_css: function() {
-		var prop;
-		var value;
-
-		for (prop in this._elem_original_css) {
-			value = this._elem_original_css[prop];
-			this.$elem.css(prop, value);
-		}
-
-		var $inner = this.$elem.find('div.modal_dialog_inner');
-		for (prop in this._inner_original_css) {
-			value = this._inner_original_css[prop];
-			$inner.css(prop, value);
-		}
-
-		if ($.browser.msie && $.browser.version < '7') {
-			// setExpression() doesn't take effect immediately, sometimes
-			this.$elem[0].style.top = document.documentElement.scrollTop;
-			this.$elem[0].style.setExpression('top', 'eval(document.documentElement.scrollTop)');
-			this.$elem[0].style.height = document.documentElement.clientHeight;
-			this.$elem[0].style.setExpression('height', 'eval(document.documentElement.clientHeight)');
-		} else {
-			this.$elem.css('height', '100%');
-		}
-	},
-
-  _set_prevent_close: function(prevent_close) {
-    this.prevent_close = prevent_close;
-    if(prevent_close)
-    {
-      this.$close_a.hide();
-    }
-    else
-    {
-      this.$close_a.show();
-    }
-  },
-
-	open: function(e) {
-		var $body = this.$elem.parents('body');
-		if ($body.length) {
-			$body.append(this.$elem);
-		}
-		this.$elem.show();
-		this._on_size_changed(true);
-
-		this.$elem.trigger('modal_dialog:opened', [e]);
-
-		this._attach();
-	},
-
-	close: function(e) {
-    if (this.prevent_close) return;
-
-    this.$elem.trigger("before_close.modal_dialog", [e]);
-
-    if (e && e.isDefaultPrevented()) return;
-
-		this._detach();
-
-		this.$elem.hide();
-
-    this.$elem.trigger("closed.modal_dialog", [e]);
-	}
-});
-
-})(jQuery);
+}(jQuery));
--- javascripts/modal_dialog_onload.js
+++ javascripts/modal_dialog_onload.js
@@ -1,33 +1,77 @@
-$(function() {
+$(function () {
 
-/*
- * For a <div class="modal_dialog" id="some_funky_dialog">, ensure that every
- * <a href="link_to_some_funky_dialog"> calls modal_dialog() on it.
- */
-$('.modal_dialog').each(function() {
-	var $dialog = $(this);
-	var dialog_id = $dialog.attr('id');
-	$('a.link_to_' + dialog_id).live("click", function(e) {
-		$dialog.modal_dialog({ event: e });
-		e.preventDefault();
-	});
-});
+  /*
+   * For a <div class="modal_dialog" id="some_funky_dialog">, ensure that every
+   * <a href="link_to_some_funky_dialog"> calls modal_dialog() on it.
+   */
+  $('.modal_dialog').each(function () {
+    var $dialog = $(this),
+      dialog_id = $dialog.attr('id');
+    $('a.link_to_' + dialog_id).live("click", function (e) {
+      $dialog.modal_dialog({ event: e });
+      e.preventDefault();
+    });
+    if ($dialog.closest('.pop_modal_on_load').length) {
+      $dialog.modal_dialog();
+    }
+  });
 
-$('a.remote_modal_dialog').live('click', function(e) {
-  e.preventDefault();
-  $.open_remote_modal_dialog($(this).attr('href'));
-});
+  $('a.remote_modal_dialog').live('click', function (e) {
+    e.preventDefault();
+    if ($(this).is('.disabled')) { return; }
+    var md = $.open_remote_modal_dialog(
+      $(this).attr('href'),
+      {
+        title: $(this).attr('data-modal-title'),
+        remote_modal_dialog_contents_loaded: function ($contents, options, remote_url) {
+          var $move_to_head = $contents.find('.move_to_head');
+          if ($move_to_head.length) {
+            $('head').append($move_to_head.children());
+            $move_to_head.remove();
+            delete window.seen_remote_urls[remote_url];
+          }
+        },
+        dataType: 'html', 
+        beforeSend: function (xhr) {
+          xhr.setRequestHeader("Accept", "text/javascript, */*");
+        }
+      }
+    );
+    $(this).data('modal_dialog', md);
+    md.$elem.bind('modal_dialog:remote_dialog_loaded', function () {
+      if (FB && FB.XFBML) {
+        FB.XFBML.parse();
+      }
+    });
+  });
 
-/*
- * If the page was loaded at hash "#modal_dialog:whatever_modal_dialog", open
- * that modal dialog.
- */
-var hash_regex = /^#modal_dialog:([-_a-zA-Z0-9]+)$/;
-var hash_match = hash_regex.exec(window.location.hash);
-if (hash_match) {
-	var dialog_id = hash_match[1];
-	$('#' + dialog_id).modal_dialog();
-	window.location.hash = '#';
-}
+  $('a.modal_dialog_close').live('click', function (e) {
+    e.preventDefault();
+    $(this).trigger('close.modal_dialog');
+  });
+
+  /*
+   * If the page was loaded at hash "#modal_dialog:whatever_modal_dialog", open
+   * that modal dialog.
+   */
+  var hash_regex = /^#modal_dialog:([\-_a-zA-Z0-9]+)$/,
+    remote_hash_regex = /^#remote_modal_dialog:(.*)$/,
+    hash_match = hash_regex.exec(window.location.hash),
+    remote_hash_match = remote_hash_regex.exec(window.location.hash),
+    dialog_id;
+  if (hash_match) {
+    dialog_id = hash_match[1];
+    $('#' + dialog_id).modal_dialog();
+    window.location.replace('#');
+  }
+  if (remote_hash_match) {
+    $('a.remote_modal_dialog').each(function () {
+      if (url_equals(decodeURIComponent(remote_hash_match[1]), $(this).attr('href'))) {
+        //this sucks
+        $(this).click();
+      }
+    });
+    window.location.replace('#');
+  }
 
 });
